edfa01f6fe5ce03729c729106fefe0eef3484931
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-09-02 13:46:33 +0100

    feat:layout

--- src/components/reuseables/Nav.tsx
+++ src/components/reuseables/Nav.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 function Nav() {
-  return <nav className="h-[80px] border-b !bg-black">Nav</nav>;
+  return (
+    <nav className=" px-8 lg:px-28 h-[80px] flex items-center justify-between">
+      <div className="relative h-12 w-[120px] md:w-[197px]">
+        <Image src={"/images/logo.svg"} fill alt="logo" />
+      </div>
+      <div />
+    </nav>
+  );
 }
 
 export default Nav;
--- src/layout/OnboardingLayout.tsx
+++ src/layout/OnboardingLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../components/reuseables/Nav";
+import Image from "next/image";
 
 function OnboardingLayout({
   children,
@@ -10,6 +11,13 @@ function OnboardingLayout({
     <div className="h-full bg-white w-full">
       <Nav />
       {children}
+      <Image
+        src="/images/backgroundpattern.png"
+        className="absolute top-2 left:[10%] lg:left-[20%]"
+        alt="pattern"
+        width={768}
+        height={768}
+      />
     </div>
   );
 }
