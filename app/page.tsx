ad8356184f8c9c4ad81a9fa7e0e8f989c540d1da
Favvie <favourabangwu@gmail.com>
2024-09-03 12:46:51 +0100

    feat: add email verification screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import LoginScreen from "@/src/Screens/onboarding/LoginScreen";
+import EmailVerification from "@/src/Screens/onboarding/EmailVerification";
 
 export default function Home() {
-  return <LoginScreen />;
+  // return <LoginScreen />;
+  return <EmailVerification />;
 }
