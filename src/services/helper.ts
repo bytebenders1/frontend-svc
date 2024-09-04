8d8a67672ce8bba93a4e823cf5295b9d001c01c1
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-09-04 13:06:24 +0100

    helper

--- src/services/helper.ts
+++ src/services/helper.ts
@@ -1,13 +1,10 @@
-import { AxiosError } from 'axios'
+import { AxiosError } from "axios";
 
 export function handleRequestError(error: any): never {
   if (error.response && error.response.data) {
-    // AxiosError is used to handle Axios-specific errors
-    const axiosError = error as AxiosError
-    throw axiosError.response?.data
+    const axiosError = error as AxiosError;
+    throw axiosError.response?.data;
   } else {
-    // For other types of errors (e.g., network errors)
-    console.log('error:', error)
-    throw new Error('An error occurred.')
+    throw new Error("An error occurred.");
   }
 }
