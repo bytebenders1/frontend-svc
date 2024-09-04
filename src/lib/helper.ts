0baf0cc790b263a26f62bbe7bbe858a35fc2971f
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-09-04 12:42:46 +0100

    helper

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,14 +1,12 @@
-import CryptoJS, { AES, enc } from 'crypto-js'
-
 export const truncateString = (str: string, num: number = 20): string => {
   // If the length of str is less than or equal to num
   // just return str--don't truncate it.
   if (str.length <= num) {
-    return str
+    return str;
   }
   // Return str truncated with '...' concatenated to the end of str.
-  return str.slice(0, num) + '...'
-}
+  return str.slice(0, num) + "...";
+};
 
 // export const capitalize = (str: string): string => {
 //   return str
@@ -16,57 +14,44 @@ export const truncateString = (str: string, num: number = 20): string => {
 
 export const removeDashes = (input: string): string => {
   // Split the string into words based on "-"
-  const words = input.split('-')
+  const words = input.split("-");
 
   // Capitalize the first letter of each word
   const capitalizedWords = words.map(
     (word) => word.charAt(0).toUpperCase() + word.slice(1)
-  )
+  );
 
   // Join the words into a sentence
-  const formattedString = capitalizedWords.join(' ')
+  const formattedString = capitalizedWords.join(" ");
 
-  return formattedString
-}
+  return formattedString;
+};
 
 export const extractErrorMessages = (error: any): string[] => {
-  const messages: string[] = []
+  const messages: string[] = [];
 
   const traverse = (obj: any) => {
     for (const key in obj) {
-      const value = obj[key]
+      const value = obj[key];
       if (Array.isArray(value)) {
-        messages.push(...value)
-      } else if (typeof value === 'object' && value !== null) {
-        traverse(value)
+        messages.push(...value);
+      } else if (typeof value === "object" && value !== null) {
+        traverse(value);
       }
     }
-  }
+  };
 
-  traverse(error)
+  traverse(error);
 
-  return messages
-}
-
-export const encryptObject = (data: any) => {
-  const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY as string
-  const encryptedData = AES.encrypt(JSON.stringify(data), secretKey).toString()
-
-  return encryptedData
-}
-
-export const decryptData = (encryptedData: string) => {
-  const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY as string
-  const decryptedData = AES.decrypt(encryptedData, secretKey).toString(enc.Utf8)
-  return JSON.parse(decryptedData)
-}
+  return messages;
+};
 
 export function removeEmptyValues(obj: any) {
-  const result: any = {}
+  const result: any = {};
   for (const key in obj) {
-    if (obj[key] !== undefined && obj[key] !== '') {
-      result[key] = obj[key]
+    if (obj[key] !== undefined && obj[key] !== "") {
+      result[key] = obj[key];
     }
   }
-  return result
+  return result;
 }
