37d5cea8b80ecb4fb10013559a5fa8a2bd4548bf
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-09-04 09:18:12 +0100

    update

--- src/components/reuseables/Nav.tsx
+++ src/components/reuseables/Nav.tsx
@@ -2,20 +2,22 @@
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import React from "react";
+import { Button } from "../ui/button";
+import { Menu } from "lucide-react";
 
 function Nav() {
   const pathname = usePathname();
-  console.log(pathname);
   return (
     <nav className="px-4 lg:px-10 xl:px-28 h-[80px] flex items-center justify-between">
       <div className="relative h-12 w-[150px] md:w-[197px]">
         <Image src={"/images/logo.svg"} fill alt="logo" />
       </div>
       <div>
-        {/* <h1>Home</h1>
-        <h1>Dashboard</h1>
-        <h1>Home</h1>
-        <h1>Home</h1> */}
+        {pathname.includes("/dashboard") && (
+          <Button>
+            <Menu />
+          </Button>
+        )}
       </div>
     </nav>
   );
